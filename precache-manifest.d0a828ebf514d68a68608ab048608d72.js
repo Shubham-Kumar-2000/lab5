253e96a83e806b5fe0ed92c202cf8d81cd880d62
Shubham-Kumar-2000 <41825906+Shubham-Kumar-2000@users.noreply.github.com>
2020-03-05 19:28:13 +0530

    for lab 5

---
+++ precache-manifest.d0a828ebf514d68a68608ab048608d72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0574f2e558c2d995a3a47fa2018cd2d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1109d012024e78c86dd3",
+    "url": "/static/css/main.3be27483.chunk.css"
+  },
+  {
+    "revision": "1171797d2110b3ca0b60",
+    "url": "/static/js/2.e5e0e516.chunk.js"
+  },
+  {
+    "revision": "1109d012024e78c86dd3",
+    "url": "/static/js/main.e6b9bb9a.chunk.js"
+  },
+  {
+    "revision": "980dc83a80998fc2b3bc",
+    "url": "/static/js/runtime-main.03711cdb.js"
+  }
+]);
